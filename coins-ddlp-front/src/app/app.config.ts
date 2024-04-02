1769f0465de589f9942eef257592610881f44a6b
Darío de la Poza <dariodelapoza@Mac-mini-M2-Pro.local>
2024-04-02 19:25:52 +0200

    config firebase

--- coins-ddlp-front/src/app/app.config.ts
+++ coins-ddlp-front/src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"coinsddlp-back","appId":"1:684451770045:web:5bb64e0ead08b97f2c91d4","storageBucket":"coinsddlp-back.appspot.com","apiKey":"AIzaSyB-1dCHmVmJSb4IiR3GPN5ys5l4YT4RlWU","authDomain":"coinsddlp-back.firebaseapp.com","messagingSenderId":"684451770045"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
