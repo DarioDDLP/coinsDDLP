b49523b2347b8384cdf313bf7c339c774960c7cd
Darío de la Poza <dariodelapoza@Mac-mini-M2-Pro.local>
2024-04-11 19:44:42 +0200

    feat(home, euros) layout euros table

--- coins-ddlp-front/src/app/home/euros/euros.component.ts
+++ coins-ddlp-front/src/app/home/euros/euros.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
-
-
+import { BadgeModule } from 'primeng/badge';
 @Component({
   selector: 'app-euros',
   standalone: true,
-  imports: [TableModule],
+  imports: [CommonModule, TableModule, BadgeModule],
   templateUrl: './euros.component.html',
   styleUrl: './euros.component.scss'
 })
@@ -13,22 +13,134 @@ export default class EurosComponent {
 
   products: any = [
     {
-      code: '001',
-      name: 'name',
-      category: 'category',
-      quantity: 10,
+      pais: 'España',
+      year: '1999',
+      valor_facial: '1 céntimo',
+      ceca: 'A Hamburgo',
+      descripcion: 'Catedral de Santiago de Compostela',
+      uds: 0,
     },
     {
-      code: '001',
-      name: 'name',
-      category: 'category',
-      quantity: 10,
+      pais: 'España',
+      year: '1999',
+      valor_facial: '2 centimos',
+      ceca: 'A Hamburgo',
+      descripcion: 'Catedral de Santiago de Compostela',
+      uds: 0,
     },
     {
-      code: '001',
-      name: 'name',
-      category: 'category',
-      quantity: 10,
+      pais: 'España',
+      year: '1999',
+      valor_facial: '5 céntimos',
+      descripcion: 'Catedral de Santiago de Compostela',
+      uds: 0,
+    },
+    {
+      package: 'España',
+      year: '1999',
+      valor_facial: '10 Céntimos',
+      descripcion: 'Cervantes',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '20 Céntimos',
+      descripcion: 'Cervantes',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '50 Céntimos',
+      descripcion: 'Cervantes',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '1 Euro',
+      descripcion: 'Rey Juan Carlos I',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '2 Euros',
+      descripcion: 'Rey Juan Carlos I',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '2 Euros C',
+      descripcion: 'Rey Juan Carlos I de España',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '1 céntimo',
+      ceca: 'A Hamburgo',
+      descripcion: 'Catedral de Santiago de Compostela',
+      uds: 0,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '2 centimos',
+      ceca: 'A Hamburgo',
+      descripcion: 'Catedral de Santiago de Compostela',
+      uds: 0,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '5 céntimos',
+      descripcion: 'Catedral de Santiago de Compostela Catedral de Santiago de Compostela',
+      uds: 0,
+    },
+    {
+      package: 'España',
+      year: '1999',
+      valor_facial: '10 Céntimos',
+      descripcion: 'Cervantes',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '20 Céntimos',
+      descripcion: 'Cervantes',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '50 Céntimos',
+      descripcion: 'Cervantes',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '1 Euro',
+      descripcion: 'Rey Juan Carlos I',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '2 Euros',
+      descripcion: 'Rey Juan Carlos I',
+      uds: 1,
+    },
+    {
+      pais: 'España',
+      year: '1999',
+      valor_facial: '2 Euros C',
+      descripcion: 'Rey Juan Carlos I de España',
+      uds: 1,
     }
   ]
 
