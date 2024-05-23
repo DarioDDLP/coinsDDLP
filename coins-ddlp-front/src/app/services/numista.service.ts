858638f0c985be7406719425494b234bfb7a72f4
Darío de la Poza <dariodelapoza@Mac-mini-M2-Pro.local>
2024-05-23 18:22:45 +0200

    fix(numista-service): refactor services new syntax

--- coins-ddlp-front/src/app/services/numista.service.ts
+++ coins-ddlp-front/src/app/services/numista.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NumistaService {
+  // Services
+  private _http = inject(HttpClient);
 
   url: string = 'https://api.numista.com/api/v3/types/'
   apiKey: string = 'ZWVN8Q87MJXAIXGt0I85MCvByduI7c6MioDnA6ZY'
-
-  constructor(private _http: HttpClient) { }
 
   getCoinByIdNum(idNum: string): Observable<any> {
     const headers = new HttpHeaders({
